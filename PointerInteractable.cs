e149f285bcb89bbd5f6de566d1da29070136e802
Russell S <32141327+Bettaxis@users.noreply.github.com>
2021-08-16 11:32:44 -0400

    Helper Scripts that work with SteamVR Laser Pointer
    Wrapper sits on the same hand with the SteamVR_LaserPointer and Pointer Interactable sits on the game object you want to interact with the laser pointer with.
    
    Note that the game object or UI button has to have a Collider -> is Trigger on.

---
+++ PointerInteractable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+// Simple script that works in tandem with the LaserPointerWrapper to convert Pointer actions to regular Unity Interaction Events
+public class PointerInteractable : MonoBehaviour, IPointerClickHandler
+{
+    public UnityEvent OnClick, PointerOut, PointerIn;
+
+   public void OnPointerClick(PointerEventData eventData)
+   {
+       OnClick.Invoke();
+   }
+
+   public void OnPointerOut(PointerEventData eventData)
+   {
+       PointerOut.Invoke();
+   }
+
+   public void OnPointerIn(PointerEventData eventData)
+   {
+       PointerIn.Invoke();
+   }
+}
